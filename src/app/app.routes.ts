f728ba0c0f6db9de63e1764adee3c33d0a09604c
ABHIJITHBYJUMINI <abhijithbm93@gmail.com>
2024-10-01 09:55:23 +0530

    created form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MyFormComponent } from './my-form/my-form.component'; // Adjust the path to where the component is located
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'myform', pathMatch: 'full' },  // Redirect root to myform
+  { path: 'myform', component: MyFormComponent },          // Route to MyFormComponent
+  { path: '**', redirectTo: 'myform' }                     // Wildcard route to redirect any unknown path to myform
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
